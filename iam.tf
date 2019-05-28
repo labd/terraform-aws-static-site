2e795d8a1693939e8fcaabb2254dd62d70e89aad
Mike Dingjan <m.dingjan@labdigital.nl>
2019-05-28 12:22:42 +0200

    Add count to iam role

---
+++ iam.tf
@@ -0,0 +1,22 @@
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type = "Service"
+
+      identifiers = [
+        "lambda.amazonaws.com",
+        "edgelambda.amazonaws.com",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  count              = "${length(var.authentication) > 0 ? "1" : "0"}"
+  name               = "${var.name}-lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.lambda.json}"
+}
