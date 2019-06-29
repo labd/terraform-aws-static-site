434c00b860394af39bbb914f6391adb215f19ab9
Mike Dingjan <m.dingjan@labdigital.nl>
2019-06-29 15:37:52 +0200

    Fix typo

--- waf.tf
+++ waf.tf
@@ -1,5 +1,5 @@
 resource "aws_waf_web_acl" "main" {
-  count       = "${lenght(var.aws_waf_rule_id) > 0 ? "${var.enabled ? 1 : 0}" : 0}"
+  count       = "${length(var.aws_waf_rule_id) > 0 ? "${var.enabled ? 1 : 0}" : 0}"
   name        = "${var.name}"
   metric_name = "${replace(var.name, "-", "")}"
 
