9f8fc681e295a379b5303847ab94d6f8c247c92a
Tim Leguijt <t.leguijt@labdigital.nl>
2020-10-21 22:16:18 +0200

    Add bucket and cloudfront ARN output

--- output.tf
+++ output.tf
@@ -17,3 +17,11 @@ output "domain_name" {
 output "hosted_zone_id" {
   value = length(local.hosted_zone_ids) == 0 ? "" : local.hosted_zone_ids[0]
 }
+
+output "bucket_arn" {
+  value = var.enabled ? aws_s3_bucket.website.0.arn : ""
+}
+
+output "distribution_arn" {
+  value = !var.enabled ? "" : length(var.authentication) > 0 ? aws_cloudfront_distribution.cloudfront_basicauth.0.arn : aws_cloudfront_distribution.cloudfront.0.arn
+}
