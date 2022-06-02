c40b63d0cea75512b2b1a4b32f9e147235cc8de8
Patrick Pepels <p.pepels@labdigital.nl>
2022-06-02 13:46:59 +0200

    removed dependency on deprecated template module

--- lambda.tf
+++ lambda.tf
@@ -1,19 +1,12 @@
-data "template_file" "basicauth" {
-  count    = length(var.authentication) > 0 ? var.enabled ? 1 : 0 : 0
-  template = "${file("${path.module}/templates/basicauth.js")}"
-
-  vars = {
-    authentication = "${jsonencode(var.authentication)}"
-  }
-}
-
 data "archive_file" "basicauth" {
   count       = length(var.authentication) > 0 ? var.enabled ? 1 : 0 : 0
   type        = "zip"
   output_path = "./files/lambda/${var.name}-basicauth.zip"
 
   source {
-    content  = "${data.template_file.basicauth.0.rendered}"
+    content = templatefile("${path.module}/templates/basicauth.js", {
+      authentication = "${jsonencode(var.authentication)}"
+    })
     filename = "index.js"
   }
 }
@@ -26,6 +19,6 @@ resource "aws_lambda_function" "basicauth" {
   role             = aws_iam_role.lambda.0.arn
   handler          = "index.handler"
   source_code_hash = data.archive_file.basicauth.0.output_base64sha256
-  runtime          = "nodejs10.x"
+  runtime          = "nodejs16.x"
   publish          = true
 }
