6010e5a413e1581246b3a367fc1dfca107b0e10e
Alec Siemerink <a.siemerink@labdigital.nl>
2023-01-31 09:26:58 +0100

    fix Warning: Redundant empty provider block

--- providers.tf
+++ providers.tf
@@ -1,5 +1,11 @@
-provider "aws" {}
-
-provider "aws" {
-  alias = "useast"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      configuration_aliases = [
+        aws,
+        aws.useast
+      ]
+    }
+  }
 }
